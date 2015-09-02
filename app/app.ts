35a4c1b30b9f1aa54a85e419dc0b2569e9ec0efc
Fabio <fubu_wu@hotmail.com>
2015-09-02 08:41:00 +0200

    intial commit

---
+++ app/app.ts
@@ -0,0 +1,12 @@
+class PositionSetter {
+	
+	
+	
+	
+	constructor( public distance : number )
+	{
+		
+	}
+	
+	Move: (name: string) => number = (name) => { return name.length};
+}
---
+++ app/scripts/app.js
@@ -0,0 +1,8 @@
+var PositionSetter = (function () {
+    function PositionSetter(distance) {
+        this.distance = distance;
+        this.Move = function (name) { return name.length; };
+    }
+    return PositionSetter;
+})();
+//# sourceMappingURL=app.js.map
