4a3c0f23dfd66e5cf26474aa07b6dadbd36af0f4
Fabio <fubu_wu@hotmail.com>
2015-09-07 09:13:03 +0200

    Edit gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,21 +5,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('sadfss', function() {
-    gulp.src('*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest(function(f) {
-            return f.base;
-        }))
-});
 
-gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
+gulp.task('sass', function() {
+    gulp.src('app/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('app/content/'));
+        .pipe(gulp.dest('app/'));
 });
 
-gulp.task('build', function() {
+gulp.task('default', function() {
     gulp.watch('app/**/*.scss', ['sass']);
 });
 
